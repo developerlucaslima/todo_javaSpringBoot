195f4c9f6134429d9cddb675dfd453ad1011c9db
developerlucaslima <developerlucaslima@gmail.com>
2023-10-12 12:04:18 -0300

    UserModel

--- src/main/java/br/com/devlucaslima/todolist/user/UserController.java
+++ src/main/java/br/com/devlucaslima/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.devlucaslima.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
   
+  @Autowired
+  private IUserRepository userRepository;
 
-  public void create(){
-    
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUserName(userModel.getUsername());
   }
 }
