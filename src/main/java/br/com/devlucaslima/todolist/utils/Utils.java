3105bd87d357a4e40c95cb096b0f62453e2c9c12
developerlucaslima <developerlucaslima@gmail.com>
2023-10-16 15:32:35 -0300

    Utils - get null properties to replace

--- src/main/java/br/com/devlucaslima/todolist/utils/Utils.java
+++ src/main/java/br/com/devlucaslima/todolist/utils/Utils.java
@@ -1,5 +1,29 @@
 package br.com.devlucaslima.todolist.utils;
 
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
 public class Utils {
-  
+
+  public String[] getNullPropertiesNames(Object source) {
+    final BeanWrapper src = new BeanWrapperImpl(source);
+
+    PropertyDescriptor[] propertyDescriptors = src.getPropertyDescriptors();
+
+    Set<String> emptyNames = new HashSet<>();
+
+    for(PropertyDescriptor pd: propertyDescriptors) {
+      Object srcValue = src.getPropertyValue(pd.getName());
+      if(srcValue == null) {
+        emptyNames.add(pd.getName());
+      }
+    }
+
+    String[] result = new String[emptyNames.size()];
+    return emptyNames.toArray(result);
+  }
 }
